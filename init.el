cd6fc434e0bda5ae991d314f407bd4dce80e3cc4
Weverson Nascimento <weverson.sn@gmail.com>
2020-06-24 15:55:55 -0300

    Add project manager

--- init.el
+++ init.el
@@ -76,10 +76,6 @@
      (ac-config-default)
      (global-auto-complete-mode t)))
 
-(use-package neotree
-  :ensure t
-  :bind (("C-/" . 'neotree-toggle)))
-
 (use-package all-the-icons
   :ensure t)
 
@@ -103,12 +99,27 @@
 	 ("C-x b" . helm-mini))
   :config (helm-mode t))
 
+(use-package projectile
+  :ensure t
+  :config
+  (define-key projectile-mode-map (kbd "C-x p") projectile-command-map)
+  (projectile-mode t))
+
+(use-package helm-projectile
+  :ensure t
+  :config
+  (helm-projectile-on))
+
+(use-package neotree
+  :ensure t
+  :bind (("C-x t" . 'neotree-toggle)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package))))
+ '(package-selected-packages (quote (projectile use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
