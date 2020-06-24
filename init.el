1cd7fdaa80b6303fa2fefbf5d6eb5080c7f50bd2
Weverson Nascimento <weverson.sn@gmail.com>
2020-06-24 18:06:23 -0300

    Clojure config

--- init.el
+++ init.el
@@ -33,14 +33,14 @@
 (global-linum-mode t)
 
 ;; show ~ at the end of the file for empty lines
-;;(setq-default indicate-empty-lines t)
+(setq-default indicate-empty-lines t)
 
-;;(when (not indicate-empty-lines)
-;;  (toggle-indicate-empty-lines))
+(when (not indicate-empty-lines)
+  (toggle-indicate-empty-lines))
 
-;;(progn
-;;    (define-fringe-bitmap 'tilde [0 0 0 113 219 142 0 0] nil nil 'center)
-;;    (setcdr (assq 'empty-line fringe-indicator-alist) 'tilde))
+(progn
+    (define-fringe-bitmap 'tilde [0 0 0 113 219 142 0 0] nil nil 'center)
+    (setcdr (assq 'empty-line fringe-indicator-alist) 'tilde))
 
 ;; if you close a buffer, it remembers where you were in the file
 (setq-default save-place t)
@@ -49,6 +49,9 @@
 
 ;; Highlight current line.
 (global-hl-line-mode t)
+
+;; Save files backup with ~ in a backup folder
+(setq backup-directory-alist `(("." . "~/.emacs.d/backup")))
 
 ;; Require and initialize `package`.
 (require 'package)
@@ -84,7 +87,7 @@
   :config
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
-  (load-theme 'doom-one t)
+  (load-theme 'doom-vibrant t)
   (doom-themes-neotree-config)
   (doom-themes-visual-bell-config)
   (doom-themes-org-config))
@@ -113,6 +116,28 @@
 (use-package neotree
   :ensure t
   :bind (("C-x t" . 'neotree-toggle)))
+
+(use-package cider
+  :ensure t)
+
+(use-package spaceline-all-the-icons
+  :ensure t
+  :config
+  (spaceline-all-the-icons--setup-package-updates) ;; Enable package update indicator
+  (spaceline-all-the-icons--setup-git-ahead)       ;; Enable # of commits ahead of upstream in git
+  (spaceline-all-the-icons--setup-neotree))         ;; Enable Neotree mode line
+
+;; spaceline configuration
+(use-package spaceline
+  :ensure t
+  :config
+  (setq-default mode-line-format '("%e" (:eval (spaceline-ml-main)))))
+
+(use-package spaceline-config
+  :ensure spaceline
+  :config
+  (spaceline-helm-mode 1)
+  (spaceline-emacs-theme))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
